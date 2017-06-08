63f2a5e3ece21ab19c90d510ab2ede48309e9181
aaric <vipaaric@gmail.com>
2017-06-08 13:47:10 +0800

    parse 0x1605

--- rooster-achieve/src/test/java/com/incarcloud/rooster/landu/P0x1605ParametersDerivedFromTheServer.java
+++ rooster-achieve/src/test/java/com/incarcloud/rooster/landu/P0x1605ParametersDerivedFromTheServer.java
@@ -13,14 +13,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
- * 3.1.3 从服务器取得参数(0x1603)<br>
- *     格式：【数据头信息】+【车辆信息】+【模块信息】+【执行动作初值】<br>
- * <i>注意：回复数据比较复杂，待解决</i>
+ * 3.1.4 上传调试数据(0x1605)<br>
+ *     格式：【命令字】+【OBD 串号（设备号）】+【TripID】+【VID】+【VIN 码】+【取得检测数据时间戮】+【数据内容】<br>
+ * <i>注：未找到测试数据，【数据内容】待定</i>
  *
- * @author Aaric, created on 2017-06-08T11:02.
+ * @author Aaric, created on 2017-06-08T11:47.
  * @since 1.0-SNAPSHOT
  */
-public class P0x1603ParametersDerivedFromTheServer {
+public class P0x1605ParametersDerivedFromTheServer {
 
     private ByteBuf buffer;
 
@@ -48,9 +48,9 @@ public class P0x1603ParametersDerivedFromTheServer {
         int offset;
         // 1.丢弃协议头
         buffer.skipBytes(8);
-        // 2.判断命令字-0x1603
+        // 2.判断命令字-0x1605
         offset = buffer.readerIndex();
-        if(0x16 == buffer.getByte(offset) && 0x03 == buffer.getByte(offset+1)) {
+        if(0x16 == buffer.getByte(offset) && 0x05 == buffer.getByte(offset+1)) {
             // 丢弃命令字
             buffer.skipBytes(2);
 
@@ -70,30 +70,13 @@ public class P0x1603ParametersDerivedFromTheServer {
             String vin = DataPackUtil.readString(buffer);
             System.out.printf("vin: %s\n", vin);
 
-            // 7.模块信息
-            // 格式：【模块信息】::=【硬件版本号】+【固件版本号】+【软件版本号】+【诊断程序类型】
-            // 7.1 硬件版本号
-            String hardwareVersionNumber = DataPackUtil.readString(buffer);
-            System.out.printf("hardwareVersionNumber: %s\n", hardwareVersionNumber);
+            // 7.取得检测数据时间戳
+            String receiveDate = DataPackUtil.readString(buffer);
+            System.out.printf("receiveDate: %s\n", receiveDate);
 
-            // 7.2 固件版本号
-            String firmwareVersionNumber = DataPackUtil.readString(buffer);
-            System.out.printf("firmwareVersionNumber: %s\n", firmwareVersionNumber);
-
-            // 7.3 软件版本号
-            String softwareVersionNumber = DataPackUtil.readString(buffer);
-            System.out.printf("softwareVersionNumber: %s\n", softwareVersionNumber);
-
-            // 7.4 诊断程序类型
-            int diagnosticProgramType = DataPackUtil.readByte(buffer);
-            System.out.printf("diagnosticProgramType: %s\n", diagnosticProgramType);
-
-            // 8.执行动作初值
-            // 格式：【执行动作初值】::=【恢复出厂设置序号】
-            int restoreFactorySettingsSerialNumber = DataPackUtil.readByte(buffer);
-            System.out.printf("restoreFactorySettingsSerialNumber: %s\n", restoreFactorySettingsSerialNumber);
+            // 8.0数据内容
+            // 格式：【数据内容】：：=十六进制数据，长度由数据包长度决定
         }
-
         System.out.println("-------------------------end");
         System.out.printf("readerIndex: %d, writerIndex: %d\n", buffer.readerIndex(), buffer.writerIndex());
     }
