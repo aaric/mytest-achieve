71c9ca1ab6f32ada1b1317cd767f3e6ab4fc738f
aaric <vipaaric@gmail.com>
2017-06-12 16:00:45 +0800

    update mns-achieve

--- mns-achieve/src/test/java/com/github/aaric/achieve/mns/MNSTest.java
+++ mns-achieve/src/test/java/com/github/aaric/achieve/mns/MNSTest.java
@@ -1,5 +1,12 @@
 package com.github.aaric.achieve.mns;
 
+import com.aliyun.mns.client.CloudAccount;
+import com.aliyun.mns.client.CloudQueue;
+import com.aliyun.mns.client.MNSClient;
+import com.aliyun.mns.common.http.ClientConfiguration;
+import com.aliyun.mns.model.Message;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,12 +23,62 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest
 public class MNSTest {
 
-
     @Value("${aliyun.accessKeyId}")
-    private String accessKeySecret;
+    private String accessId;
+
+    @Value("${aliyun.accessKeySecret}")
+    private String accessKey;
+
+    @Value("${aliyun.MNS.endpoint}")
+    private String accountEndpoint;
+
+    @Value("${aliyun.MNS.maxConnections}")
+    private int maxConnections;
+
+    @Value("${aliyun.MNS.maxConnectionsPerRoute}")
+    private int maxConnectionsPerRoute;
+
+    @Value("${aliyun.MNS.queueName}")
+    private String queueName;
+
+    @Value("${aliyun.MNS.topicName}")
+    private String topicName;
+
+    protected MNSClient client;
+
+    @Before
+    public void begin() throws Exception {
+        ClientConfiguration config = new ClientConfiguration();
+        config.setMaxConnections(maxConnections);
+        config.setMaxConnectionsPerRoute(maxConnectionsPerRoute);
+        CloudAccount account = new CloudAccount(accessId, accessKey, accountEndpoint, config);
+        client = account.getMNSClient();
+    }
+
+    @After
+    public void end() throws Exception {
+        client.close();
+    }
 
     @Test
-    public void test() throws Exception {
-        System.err.println(accessKeySecret);
+    public void testProducer() throws Exception {
+        CloudQueue cloudQueue = client.getQueueRef(queueName);
+        Message message = new Message();
+        message.setMessageBody("test msg");
+        message = cloudQueue.putMessage(message);
+        message.setPriority(8);
+        System.out.printf("messageId: %s\n", message.getMessageId());
+    }
+
+    @Test
+    public void testConsumer() throws Exception {
+        CloudQueue cloudQueue = client.getQueueRef(queueName);
+
+        // Get
+        Message message = cloudQueue.popMessage();
+        System.out.println(message.getMessageBodyAsString());
+
+        // Delete
+        //cloudQueue.deleteMessage(message.getReceiptHandle());
     }
 }
