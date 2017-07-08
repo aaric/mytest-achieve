5d65467af97d448d82b6bd7b6d5d9786f7b1b365
aaric <vipaaric@gmail.com>
2017-07-08 12:25:23 +0800

    add elasticsearch-achieve

---
+++ elasticsearch-achieve/src/test/java/com/github/aaric/achieve/elasticsearch/ElasticSearchTest.java
@@ -0,0 +1,17 @@
+package com.github.aaric.achieve.elasticsearch;
+
+import org.junit.Test;
+
+/**
+ * ElasticSearchTest
+ *
+ * @author Aaric, created on 2017-07-04T11:11.
+ * @since 1.0-SNAPSHOT
+ */
+public class ElasticSearchTest {
+
+    @Test
+    public void test() {
+
+    }
+}
