e5be2fa26972e7bc670e62f6f53f7e42bada3d69
aaric <vipaaric@gmail.com>
2017-10-09 09:01:52 +0800

    update template-achieve

---
+++ template-achieve/src/main/java/com/github/aaric/achieve/Application.java
@@ -0,0 +1,15 @@
+package com.github.aaric.achieve;
+
+/**
+ * Spring Boot Launcher.
+ *
+ * @author Aaric, created on 2017-09-01T15:31.
+ * @since 1.0-SNAPSHOT
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
